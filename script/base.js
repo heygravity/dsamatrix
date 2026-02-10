e4fa5ebb771d728b70dadeef8e65dc6ba2c3996e
Pritish Rajpurohit <workforpritish@gmail.com>
2026-02-10 22:40:16 +0530

    + Created authentication using Firebase Auth and Firestore for user profiles + Implemented session management with localStorage to persist user sessions across page reloads + Added navigation logic to redirect unauthenticated users to the login page and authenticated users to the arena + Updated UI to reflect user authentication status and display user profile information

---
+++ script/base.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCx01jmC2CA-cLqZWUwocv-RuJfdqA_aY8",
+    authDomain: "heygravity-ba1b3.firebaseapp.com",
+    projectId: "heygravity-ba1b3",
+    storageBucket: "heygravity-ba1b3.firebasestorage.app",
+    messagingSenderId: "278985291763",
+    appId: "1:278985291763:web:501a2207f0868b39954486",
+    measurementId: "G-KBP76GF5W1"
+};
+
+export { firebaseConfig };
